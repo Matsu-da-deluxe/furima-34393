06f19c09b79d66224abf5f0ebfd2aa51b00e62c4
Matsu-da-deluxe <j.takez03@gmail.com>
2021-03-03 18:36:24 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,18 @@
 class ItemsController < ApplicationController
   def index 
   end
+
+  def create
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :image, :text).merge(user_id: current_user.id)
+  end
+
 end
