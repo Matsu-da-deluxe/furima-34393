4ebeac7a638b9bb99077a56a7becd210672a5305
Matsu-da-deluxe <j.takez03@gmail.com>
2021-03-03 16:10:19 +0900

    userモデルの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,17 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_one :itimes
+  has_one :itemes
   has_one :buyers
   
-    validates :nickname, presence: true
-    validates :email, presence: true
-    validates :birthday, presence: true
-
+    
     with_options presence: true do
+      validates :nickname
+      validates :email
+      validates :birthday
+
       validates :password, format: {with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/ ,message: " Include both letters and numbers"}
-      validates :f_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-      validates :l_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+      validates :f_name, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "is invalid. Input full-width characters."}
+      validates :l_name, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "is invalid. Input full-width characters."}
       validates :f_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
       validates :l_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
   end
