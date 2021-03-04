40e5e763794e8ac1e03b149c162c85deb9519dc8
Matsu-da-deluxe <j.takez03@gmail.com>
2021-03-04 19:32:31 +0900

    バリデーションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,9 +4,9 @@ class ItemsController < ApplicationController
   end
 
   def new
-    @condition = Condition.all
+    @item = Item.new
   end
-  
+
   def create
     Item.create(item_params)
   end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,30 @@
 class Item < ApplicationRecord
-
-  has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
+  belongs_to :category
+  belongs_to :status
+  belongs_to :ship_fee
+  belongs_to :area
+  belongs_to :date
+  has_one_attached :image
+  has_one :buyer
+
+  validates :title, :text, presence: true
+  #ジャンルの選択が「--」の時は保存できないようにする
+  
+
+
+  with_options presence: true do
+    validates :price,numericality:{only_integer:true, message:'Half-width number'}
+    validates :price,numericality:{greater_than_or_equal_to:300, less_than_or_equal_to:9_999_999,message:'Out of setting range'}
+  end
+
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :status_id
+    validates :ship_fee_id
+    validates :area_id
+    validates :date_id
+  end
 end
